f50e5cd0674dded7a10b53bae38edd9795b983b0
Jason Hanggi <jason@tablexi.com>
2014-02-12 16:33:49 -0600

    [#73575] Refactor user reservation actions out of HAML
    This will pave the way for adding in process consumables.
    
    Conflicts:
    	app/helpers/reservations_helper.rb

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -8,4 +8,16 @@ module ReservationsHelper
   def reservation_pick_accessories_path(reservation)
     new_order_order_detail_accessory_path(reservation.order_detail.order, reservation.order_detail)
   end
+
+  def end_reservation_class(reservation)
+    reservation.order_detail.accessories? ? :has_accessories : nil
+  end
+
+  def reservation_actions(reservation)
+    ReservationUserActionPresenter.new(self, reservation).user_actions
+  end
+
+  def reservation_view_edit_link(reservation)
+    ReservationUserActionPresenter.new(self, reservation).view_edit_link
+  end
 end
